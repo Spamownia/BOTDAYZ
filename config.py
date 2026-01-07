a235f7139dafb6b984ab9d5ada889ab0415c44ec
Spamownia <spamownia91@gmail.com>
2026-01-07 20:33:24 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,10 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 # FTP
 FTP_HOST = os.getenv("FTP_HOST")
+FTP_PORT = int(os.getenv("FTP_PORT", "21"))  # domyślnie 21, ale u Ciebie 51421
 FTP_USER = os.getenv("FTP_USER")
 FTP_PASS = os.getenv("FTP_PASS")
-FTP_LOG_DIR = os.getenv("FTP_LOG_DIR", "/config/")  # <-- zmienione na /config/
+FTP_LOG_DIR = os.getenv("FTP_LOG_DIR", "/config/")
 
 # ID kanałów – zmień na swoje!
 CHANNEL_IDS = {

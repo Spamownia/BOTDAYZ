8e97c3d6712a4b437040f75958ba8b2dae39aa10
Spamownia <spamownia91@gmail.com>
2026-01-08 20:06:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CHANNEL_IDS = {
     "deaths":      1458887056834040012,  # <-- ZMIEŃ NA ID KANAŁU NA ŚMIERCI
     "admin":       1458887056834040012,  # <-- ZMIEŃ NA ID KANAŁU NA AKCJE ADMINA
     "chat":        1458887056834040012,                # <-- opcjonalnie ID kanału na chat w grze
-    "debug":       none,  # <-- NA CZAS TESTÓW WPISZ ID KANAŁU DEBUG (wszystkie linie logów)
+    "debug":       None,  # <-- NA CZAS TESTÓW WPISZ ID KANAŁU DEBUG (wszystkie linie logów)
 }
 
 # Ustawienia

ce312b47464cf92de5609a14db5d44fb95f859d3
Spamownia <spamownia91@gmail.com>
2026-01-11 19:32:45 +0100

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CHANNEL_IDS = {
     "deaths": 1458909797121527849,
     "admin": 1458909797121527849,
     "chat": 1459919995919143024,         # domyślny dla chatu (jeśli nie ma mapowania)
-    "debug": None                        # wyłącz debug, żeby uniknąć rate limit
+    "debug": 1458909797121527849                        # wyłącz debug, żeby uniknąć rate limit
 }
 
 # Mapowanie typów chatu DayZ → konkretne kanały Discord

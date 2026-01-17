41c76ced35002fc9023753ec04450f3680829fa2
Spamownia <spamownia91@gmail.com>
2026-01-17 15:42:46 +0100

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ FTP_HOST = os.getenv("FTP_HOST")
 FTP_PORT = int(os.getenv("FTP_PORT", "21"))
 FTP_USER = os.getenv("FTP_USER")
 FTP_PASS = os.getenv("FTP_PASS")
-FTP_LOG_DIR = "/"               # ← Zmień na właściwą ścieżkę: "/", "config/", "profiles/", "./" itp.
+FTP_LOG_DIR = "/config"               # ← Zmień na właściwą ścieżkę: "/", "config/", "profiles/", "./" itp.
 
 if not all([FTP_HOST, FTP_USER, FTP_PASS]):
     raise ValueError("Brak danych FTP!")
